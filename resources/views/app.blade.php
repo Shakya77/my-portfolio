129fb94e38c52c0c0b62a8fbcdad8973739bfbf3
shakya77 <bijanshakya145@gmail.com>
2026-02-23 20:35:11 +0545

    few changes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -41,8 +41,8 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-    @routes
     @viteReactRefresh
+    @routes
     @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
     @inertiaHead
 </head>
